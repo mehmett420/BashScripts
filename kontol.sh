c73ae2a9cb339cf54f6aeae685e83655a2c0c8e3
mehmet ozdemir <mehmetozdemir206@gmail.com>
2019-08-15 09:33:14 +0300

    Create kontol.sh

---
+++ kontol.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+read -p "disk kontrol icin 1 script calistirak icin 2 ye basiniz" secim
+if [ $secim = "1" ]
+then
+    cd ~/scripts
+    du -ah
+elif [ $secim = "2" ]
+then
+    cd ~/
+    chmod +x ~/kontrol1
+    bash ~/kontrol1
+    if [ $0 = "line" ]
+    then
+        echo "Script hatalidir."
+    else
+        echo "Scripts calisiyor."
+    fi
+fi
